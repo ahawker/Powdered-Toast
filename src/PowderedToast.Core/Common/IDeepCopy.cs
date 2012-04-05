92732c1a8935ea9d8718b67af4368113b49a810c
ahawker <andrew.r.hawker@gmail.com>
2012-04-05 07:08:37 -0700

    Added DeepCopy supported (via BinaryFormatter) for INotification objects.

---
+++ src/PowderedToast.Core/Common/IDeepCopy.cs
@@ -0,0 +1,15 @@
+﻿namespace PowderedToast
+{
+    /// <summary>
+    /// Supports a deep copy of the object via serialization.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IDeepCopy<out T>
+    {
+        /// <summary>
+        /// Makes a deep copy of the object using serialization. Implementer must be marked as [Serializable].
+        /// </summary>
+        /// <returns>Deep copy of the implementer.</returns>
+        T DeepCopy();
+    }
+}
---
+++ src/PowderedToast.Core/Common/Utilities.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace PowderedToast
+{
+    /// <summary>
+    /// Contains common helper and utility methods.
+    /// </summary>
+    internal static class Utilities
+    {
+        /// <summary>
+        /// Performs a deep copy of an <typeparamref name="T"/> object using serialization. 
+        /// <typeparamref name="T"/> must be marked as [Serializable].
+        /// </summary>
+        /// <typeparam name="T">Object type.</typeparam>
+        /// <param name="obj">Object to copy.</param>
+        /// <returns>Deep copy of object.</returns>
+        /// <remarks>
+        /// http://stackoverflow.com/questions/129389/how-do-you-do-a-deep-copy-an-object-in-net-c-specifically
+        /// </remarks>
+        internal static T DeepCopy<T>(T obj)
+        {
+            using (var stream = new MemoryStream())
+            {
+                var formatter = new BinaryFormatter();
+                formatter.Serialize(stream, obj);
+                stream.Position = 0;
+                return (T)formatter.Deserialize(stream);
+            }
+        }
+    }
+}
