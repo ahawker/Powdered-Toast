96f0ccb1efdf0883b121b6ad1f65c0f9e47b77c8
ahawker <andrew.r.hawker@gmail.com>
2012-04-05 07:20:35 -0700

    Stubbed Api object. I'm still not sure if I want a single static object that controls all user app space.

---
+++ src/PowderedToast.Core/Api/PowderedToastApi.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using PowderedToast.Notifications;
+using PowderedToast.Subscription;
+
+namespace PowderedToast.Api
+{
+    public static class PowderedToastApi
+    {
+        public static INotifier GetNotifier()
+        {
+            return null;
+        }
+
+        public static INotifier GetNotifier(string owner)
+        {
+            return null;
+        }
+
+        public static void RegisterSubscriber(INotificationSubscriber subscriber)
+        {
+        }
+
+        public static void DeregisterSubscriber(INotificationSubscriber subscriber)
+        {
+        }
+    }
+}
